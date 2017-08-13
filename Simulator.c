a6e0b3b19de5ff3c33eb646423adb1d0dab18a72
Bismeet singh <bismeetsingh352@gmail.com>
2017-08-13 08:57:46 +0000

    Updated
    Fixed the loops and random nos.

--- Simulator.c
+++ Simulator.c
@@ -1,14 +1,6 @@
-/*
-NAME:	 Simulator.c
-PROGRAM: A C Program to generate a finite number of processes having random no. of 
-		 interleaved I/O and CPU bursts of random duration using random number
-		 generating function.
-BY:		 Zaid Khan
-COMPATIBLE COMPILER: GCC 4.5.2 with C/C++ 11 option selected.
-*/
-
 #include <stdio.h> 
 #include <stdlib.h> 
+#include<time.h>
 
 //stdlib for srand() and rand().
 
@@ -22,29 +14,27 @@ int process[10][22];
 
 /*This is the random generation function for Arrival Time calculation and 
 Burst duration calculation*/
-int randomnumberforAT (int i)
+int randomnumberforAT ()
 {
-	srand(i); //Seed initialization
+	//srand(i); //Seed initialization
 	int n = 0;
-	do
-	{
-		n = rand() % 100; //Numbers will be from 1 - 100
-	}while(n == 0); //To ensure no number generated is 0
+	
+		n = rand() % 99+1; //Numbers will be from 1 - 100
+	 //To ensure no number generated is 0
 
 	return n;
 }
 
 
 /*This is the random generation function for Number of CPU bursts*/
-int randomnumber (int i)
+int randomnumber ()
 {
-	srand(i);
+	//srand(i);
 
 	int n = 0;
-	do
-	{
-		n = rand() % 10;
-	}while(n == 0);
+	
+		n = rand() % 9+1;
+	
 
 	return n;
 }
@@ -63,7 +53,7 @@ void Arrival_Assignment()
 
 	process [0][1] = 0;
 	for (i = 1; i < 10; i++)
-		process [i][1] = process [i - 1][1] + randomnumberforAT(i * i * i + 35);
+		process [i][1] = process [i - 1][1] + randomnumberforAT();
 		//The i 'cube' is just for adding salt i.e. making it more random.
 		//35 is the sum of the individual digits of my birthdate.
 }
@@ -74,7 +64,7 @@ void NBurst_Assignment()
 	int i;
 
 	for (i = 0; i < 10; ++i)
-		process [i][2] = randomnumber((i + 7) * (i + 17) * 35);
+		process [i][2] = randomnumber(rand()%99+1);
 		//Prime numbers are ideal for introducing randomness
 		//from what I've learned.
 }
@@ -90,7 +80,7 @@ void Burst_Assignment()
 		n = process [i][2] * 2 + 1; //Total number of Bursts = CPU burst * 2 + 1
 		int j;
 		for (j = 0; j < n; j++)
-			process [i][3 + j] = randomnumberforAT (n * (i + 1) * (j + 1) + 35);
+			process [i][3 + j] = randomnumberforAT ();
 	}
 }
 
@@ -112,6 +102,8 @@ void print()
 //Main function
 int main()
 {
+int seed=time(NULL);
+srand(seed);
 	ID_Assignment();
 	Arrival_Assignment();
 	NBurst_Assignment();
